91d7e9f649def1e20e82c4207f549e482e221175
nihalafs11 <nihalafs11@gmail.com>
2022-09-30 15:04:33 -0400

    Started on program structure of PyCompGeo

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from tkinter import *
+import tkinter as tk
 import mpmath
 import sympy
 import pytest
 
-window=Tk()
 
-# For test
-first = 1
+class MainUI(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("PyCompGeo")
+        self.geometry('800x500+0+0')
+        self.resizable(True, True)
+        #self.iconphoto(False, tk.PhotoImage(file="assets/title_icon.png"))
+        container = tk.Frame(self, bg="#8AA7A9")
+        container.pack(side="top", fill="both", expand = True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-
-window.title('Hello World')
-window.geometry("300x200+10+20")
-window.mainloop()
-#matt was here
-#prashan was here
-#a;lskdjiwal
-#asdasdasdasdadasdim so angery
-#test again
-
-# chris was here UwU again
+if __name__ == "__main__":
+    app = MainUI()
+    app.mainloop()
